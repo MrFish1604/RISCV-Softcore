3189c86a8f2ba78fec1f4b41c742eb8fd4690315
Matthias Cabillot <matthias.cabillot@telecomnancy.net>
2024-10-29 16:15:28 +0100

    Update controleur.vhd to handle Imm

--- controleur.vhd
+++ controleur.vhd
@@ -13,7 +13,8 @@ entity controleur is
         instr: in std_logic_vector((N-1) downto 0);
         load: out std_logic:='0';
         we: out std_logic:='1';
-        aluOp: out std_logic_vector((N_OP-1) downto 0)
+        aluOp: out std_logic_vector((N_OP-1) downto 0);
+        RI_sel: out std_logic
     );
     alias funct7: std_logic_vector(6 downto 0) is instr(31 downto 25);
     alias rs2 : std_logic_vector(4 downto 0) is instr(24 downto 20);
@@ -21,9 +22,13 @@ entity controleur is
     alias funct3: std_logic_vector(2 downto 0) is instr(14 downto 12);
     alias rd : std_logic_vector(4 downto 0) is instr(11 downto 7);
     alias opcode : std_logic_vector(6 downto 0) is instr(6 downto 0);
+
+    constant R_TYPE: std_logic_vector(6 downto 0) := "0110011";
+    constant I_TYPE: std_logic_vector(6 downto 0) := "0010011";
 end controleur;
 
 architecture rtl of controleur is
 begin
-    aluOp <= funct7(5) & funct3;
+    RI_sel <= '0' when opcode=R_TYPE else '1' when opcode=I_TYPE else 'U';
+    aluOp <= funct7(5) & funct3 when opcode=R_TYPE else "0" & funct3;
 end rtl;
